b471cf0d348deb3d56d16fb2a193f85904441ab9
Casey Li <casey.li@gmail.com>
2013-03-06 11:24:38 -0500

    Change Image Uploader procesing resolution.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -30,7 +30,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :resize_to_limit => [400, 400]
+  process :resize_to_limit => [170, 150]
   #
   # def scale(width, height)
   #   # do something
