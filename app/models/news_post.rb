6f8e79d2c6bd7397be7fb01f8352282c3e29e94b
Casey Li <casey.li@gmail.com>
2014-01-14 22:22:22 -0500

    Sorted news posts by created_at.

--- app/models/news_post.rb
+++ app/models/news_post.rb
@@ -1,5 +1,5 @@
 class NewsPost < ActiveRecord::Base
-  default_scope order("updated_at DESC")
+  default_scope order("created_at DESC")
   attr_accessible :body, :image, :title
   
   mount_uploader :image, ImageUploader
