f49ff2cc617afab817096ed9f3805d0d0a5bafdf
Casey Li <casey.li@gmail.com>
2015-02-23 19:42:16 -0500

    Changed video ordering and news display.

--- app/models/video_listing.rb
+++ app/models/video_listing.rb
@@ -1,5 +1,5 @@
 class VideoListing < ActiveRecord::Base
-  default_scope order("updated_at DESC")
+  default_scope order("created_at DESC")
   attr_accessible :image, :link, :name
   
   mount_uploader :image, ImageUploader
