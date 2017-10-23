65ef8b9b8c6cf3bae9082b873b08d2b697b14eeb
Casey Li <casey.li@gmail.com>
2017-10-23 15:07:05 -0400

    Removed validation on profile.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,4 @@
 class Profile < ActiveRecord::Base
   attr_accessible :employee_number, :first_name, :last_name, :start_date
-  validates :employee_number, uniqueness: {scope: :employee_number}
   belongs_to :user
 end
