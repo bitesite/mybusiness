7a85fe359450d2f07ee35e8c61ec576da5dbff16
Casey Li <casey.li@gmail.com>
2017-12-02 01:12:35 -0500

    Finished blog post controller specs.

--- spec/controllers/blog_posts_controller_spec.rb
+++ spec/controllers/blog_posts_controller_spec.rb
@@ -1,26 +1,107 @@
 require 'rails_helper'
 
 describe BlogPostsController, type: :controller do
+
+  let(:blog_post) { create(:blog_post) }
+
   shared_examples 'a user who can view blog posts' do
     describe "GET #index" do
-      it "denies access" do
+      it "returns successfully" do
         get :index
         expect(response).to have_http_status :ok
       end
     end
   end
 
+  shared_examples 'a user who cannot manage blog posts' do
+    describe 'GET #new' do
+      it 'denies access' do
+        get :new
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'denies access' do
+        get :edit
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    describe 'POST #create' do
+      it 'denies access' do
+        post :create, blog_post: attributes_for(:blog_post)
+        expect(response).to redirect_to root_path
+      end
+
+      it 'does not create a blog post' do
+        expect {
+          post :create, bog_post: attributes_for(:blog_post)
+        }.to change(BlogPost, :count).by(0)
+      end
+    end
+
+    describe 'PUT #update' do
+      it 'denies access' do
+        blog_post
+        put :update, id: blog_post.id, blog_post: {title: "NEW TITLE!"}
+        expect(response).to redirect_to root_path
+      end
+
+      it 'does not update the blog post' do
+        blog_post
+        original_title = blog_post.title
+        put :update, id: blog_post.id, blog_post: {title: "#{original_title} V2"}
+        blog_post.reload
+        expect(blog_post.title).to eq(original_title)
+      end
+    end
+  end
+
   shared_examples 'a user who can manage blog posts' do
-    destribe 'GET #index' do
+    describe 'GET #index' do
       it 'returns a 200 OK' do
         get :index
         expect(response).to have_http_status :ok
+      end
+    end
+
+    describe 'GET #new' do
+      it 'renders the new template' do
+        get :new
+        expect(response).to render_template :new
+      end
+    end
+
+    describe 'GET #edit' do
+      it 'renders the edit template' do
+        get :edit
+        expect(response).to render_template :edit
+      end
+    end
+
+    describe 'POST #create' do
+      it 'creates a blog post' do |variable|
+        expect {
+          post :create, blog_post: attributes_for(:blog_post)
+        }.to change(BlogPost, :count).by(1)
+      end
+    end
+
+    describe 'PUT #update' do
+      it 'updates the blog post' do
+        blog_post
+        original_title = blog_post.title
+        put :update, id: blog_post.id, blog_post: {title: "#{original_title} V2"}
+        blog_post.reload
+        expect(blog_post.title).to eq("#{original_title} V2")
       end
     end
   end
 
   context 'when not signed in' do
     it_behaves_like 'a user who can view blog posts'
+    it_behaves_like 'a user who cannot manage blog posts'
   end
 
   context 'when signed in' do
@@ -29,18 +110,25 @@ describe BlogPostsController, type: :controller do
     end
 
     it_behaves_like 'a user who can view blog posts'
+    it_behaves_like 'a user who cannot manage blog posts'
   end
 
   context 'when signed in as an admin' do
     before :each do
       sign_in create(:admin_user)
     end
+
+    it_behaves_like 'a user who can view blog posts'
+    it_behaves_like 'a user who can manage blog posts'
   end
 
   context 'when signed in as staff' do
     before :each do
       sign_in create(:staff_user)
     end
+
+    it_behaves_like 'a user who can view blog posts'
+    it_behaves_like 'a user who can manage blog posts'
   end
 
   context 'when signed in as a supervisor' do
