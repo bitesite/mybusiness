e47be8662da740fae5faa50dfb08e5ca65d9d9ef
Casey Li <casey.li@gmail.com>
2018-07-27 15:47:55 -0400

    Allowed Staff to view hidden blog posts.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,7 +5,7 @@ class BlogPostsController < ApplicationController
 
   def index
     @blog_posts = BlogPost.published
-    @blog_posts = BlogPost.all if admin?
+    @blog_posts = BlogPost.all if (admin? || staff?)
     @blog_posts = @blog_posts.tagged_with(params[:tag_name]) if params[:tag_name]
     @blog_posts = @blog_posts.paginate(page: params[:page], per_page: 10)
     @title = ""
