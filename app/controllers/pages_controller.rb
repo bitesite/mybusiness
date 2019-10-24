430d7fd3e4ef79f569309a9b169452ecb44835c6
Casey Li <casey.li@gmail.com>
2019-10-24 15:22:23 -0400

    Moved case studies.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -122,64 +122,11 @@ class PagesController < ApplicationController
     @title = "Setting up your Heroku account"
   end
 
-  def international_safety
-    @title = "International Safety Website - Case Study"
-  end
-
-  def mydoma
-    @title = "Mydoma Studio Video - Case Study"
-  end
-
-  def splice
-    @title = "Splice Web Application - Case Study"
-  end
-
-  def lspark_grad
-    @title = "L-SPARK Grad Video - Case Study"
-  end
-
-  def lspark
-    @title = "L-SPARK - Case Study"
-  end
-
-  def d3m
-    @title = "Teldio D3M - Case Study"
-  end
-
-  def martello
-    @title = "Martello All Devices Video - Case Study"
-  end
-
-  def curtiss_wright
-    @title = "Curtiss-Wright Defense Solutions TLCM Video - Case Study"
-  end
-
-  def ewa
-    @title = "Enterprise Wireless Alliance Cevo GO Video - Case Study"
-  end
-
-  def solink
-    @title = "Solink Explainer Video - Case Study"
-  end
-
-  def christine_kelly
-    @title = "Christine Kelly PHD Web Application"
-  end
-  
-  def filefacets
-    @title = "FileFacets How it works Video"
-  end
-
-  def inspec_homes
-    @title = "Inspec Homes Web Application"
-  end
-
   def staff_dashboard
     if !can?(:view, :staff_dashboard)
       redirect_to root_path 
     else
       @title = "Staff Dashboard"
-
     end
   end
 
