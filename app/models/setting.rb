0b508742b7938d8cb56ac53eff74e6a48d27a865
Casey Li <casey.li@gmail.com>
2021-05-18 14:59:20 -0400

    Added products page.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -2,5 +2,17 @@ class Setting < ApplicationRecord
   SETTING_NAMES = ["contact_confirmation_message",
                    "featured_blog_post_ids",
                   "freshbooks_api_client_id",
-                  "freshbooks_api_client_secret"].sort
+                  "freshbooks_api_client_secret",
+                  "products_content"].sort
+  
+  def self.value_for(name, default_value = nil)
+    setting = Setting.find_by(name: name)
+
+    if setting
+      setting.value
+    else
+      nil || default_value
+    end
+  end
+  
 end
