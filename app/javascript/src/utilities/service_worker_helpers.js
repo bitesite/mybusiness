f02c5f7f2b0256729ceef974b9e3309de669c14b
Jack wu <jackwu833016@gmail.com>
2021-11-02 16:04:24 -0400

    fix linting issue

--- app/javascript/src/utilities/service_worker_helpers.js
+++ app/javascript/src/utilities/service_worker_helpers.js
@@ -1,4 +1,4 @@
-function registerServiceWorker(onRegister) {
+const registerServiceWorker(onRegister)  => {
   if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/serviceworker.js', { scope: './' }).then(() => {
       if (typeof onRegister === 'function') {
