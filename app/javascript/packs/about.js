939a266e5c332e9ec51e7f9c38049c3de558e63a
Anna Baranova <asbaranova1@gmail.com>
2022-07-15 14:54:20 -0400

    add grid

--- app/javascript/packs/about.js
+++ app/javascript/packs/about.js
@@ -5,8 +5,8 @@ import aboutHeroImage from '../../assets/images/about-hero-image.png';
 
 const AboutPage = () => (
   <div className="about-page fgs-al  fgs-al-v frs-al-g-10">
-    <div className="about-page-header fgs-al  fgs-al-v frs-al-g-64">
-      <div className="about-header-title">About Us</div>
+    <div className="about-page-header fgs-al  fgs-al-v fgs-al-g-60">
+      <h1 className="about-header-title">About Us</h1>
       <img src={aboutHeroImage} alt="about-main-pic" className="about-image" />
       <div className="about-content">
         BiteSite was founded in 2012 in Ottawa, Canada and started as a general service company offering a wide range of services
@@ -16,11 +16,13 @@ const AboutPage = () => (
         platforms, we specialize in full-stack Web Applications and iOS and Android applications.
       </div>
     </div>
-    <div className="about-page-beliefs">
+    <div className="about-page-beliefs  fgs-al  fgs-al-v fgs-al-g-60 fgs-al-align-items-center fgs-al-justify-content-flex-center">
       <div className="about-belief-title">Our Beliefs</div>
-      <div className="about-belief-content">
-        <div className="about-belief-item fgs-al fgs-al-v frs-al-g-10">
-          <div className="about-belief-item-title">Transparency</div>
+      <div className="about-belief-content  fgs-al  fgs-al-h frs-al-g-30 fgs-al-align-items-flex-start fgs-al-p-10">
+        <div className="about-belief-item fgs-al fgs-al-v fgs-al-g-20  fgs-al-align-items-flex-start fgs-al-p-10">
+          <div className="about-belief-item-title">
+            <div>Transparency</div>
+          </div>
           <div className="about-belief-item-content">
             We place honesty amongst ourselves and with our clients above business, money, or conflict. At BiteSite, we don’t shy
             away from speaking our mind, because we know the best teams are made up of authentic people who value collaboration
@@ -28,8 +30,10 @@ const AboutPage = () => (
             never surprised.
           </div>
         </div>
-        <div className="about-belief-item fgs-al fgs-al-v frs-al-g-10">
-          <div className="about-belief-item-title">Flexibility</div>
+        <div className="about-belief-item fgs-al fgs-al-v fgs-al-g-20  fgs-al-align-items-flex-start fgs-al-p-10">
+          <div className="about-belief-item-title">
+            <div>Flexibility</div>
+          </div>
           <div className="about-belief-item-content">
             The reality is, it's very hard to predict how well a product, or even a client relationship, will do. Whether it be
             personalities, working style, or environment, there are a lot of factors that have to line up for a successful
@@ -37,8 +41,10 @@ const AboutPage = () => (
             unnecessary, and add our own processes when needed.
           </div>
         </div>
-        <div className="about-belief-item fgs-al fgs-al-v frs-al-g-10">
-          <div className="about-belief-item-title">Predictability/ Collaborative Progress/ Iterations</div>
+        <div className="about-belief-item fgs-al fgs-al-v fgs-al-g-20  fgs-al-align-items-flex-start fgs-al-p-10">
+          <div className="about-belief-item-title">
+            <div>Predictability/ Collaborative Progress/ Iterations</div>
+          </div>
 
           <div className="about-belief-item-content">
             We work best when there is a well structured, defined process that still allows for agile flexibility. We believe that
