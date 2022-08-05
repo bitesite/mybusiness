7ab81d3f758f700164266b95458b0886c96d186b
Angela Choi <angelachoiburns@Angelas-MacBook-Pro.local>
2022-08-05 14:33:37 -0400

    added info to services page

--- app/javascript/packs/services.jsx
+++ app/javascript/packs/services.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-const servicesPage = () => {
-  <h1>Services Page</h1>;
-};
-export default servicesPage;
+const ServicesPage = () => <div className="component-services" />;
+
+export default ServicesPage;
 
 document.addEventListener('DOMContentLoaded', () => {
   const element = document.getElementById('services-page-mount-point');
 
   if (element) {
-    ReactDOM.render(<servicesPage />, element);
+    ReactDOM.render(<ServicesPage />, element);
   }
 });
