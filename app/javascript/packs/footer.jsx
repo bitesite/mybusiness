40d6b2354dd8a1ea3368852f662acd7aab22815d
Nar Raeewal <nraeewal@Nars-MacBook-Pro.local>
2022-08-05 12:05:46 -0600

    pr comments

--- app/javascript/packs/footer.jsx
+++ app/javascript/packs/footer.jsx
@@ -11,44 +11,44 @@ const Footer = () => (
       <div className="company-info caption-light fgs-ali fgs-al fgs-al-v fgs-al-g-10 ">
         <span className="title caption-bold fgs-ali">BiteSite Inc.</span>
 
-        <div className="info fgs-al fgs-ali  fgs-al-v fgs-al-g-6">
-          <span className="info fgs-ali">© 2021 BiteSite Inc.</span>
+        <div className="info caption-light fgs-al fgs-ali  fgs-al-v fgs-al-g-6">
+          <span className="caption-light fgs-ali">© 2021 BiteSite Inc.</span>
 
-          <span className="info fgs-ali">
+          <span className="caption-light fgs-ali">
             204 - 78 George St.
             <br />
             Ottawa, ON <br /> K1N 5W1
           </span>
-          <span className="info fgs-ali">info@bitesite.ca</span>
+          <span className="caption-light fgs-ali">info@bitesite.ca</span>
         </div>
       </div>
 
       <div className="links fgs-al fgs-ali  fgs-al-h fgs-al-align-items-flex-start fgs-al-g-60 ">
         <div className="company-links caption-medium fgs-ali fgs-al fgs-al-v fgs-al-g-6">
           <span className="links-title fgs-ali">COMPANY</span>
-          <Link path="/services" className="fgs-ali">
+          <Link path="/services" target="_blank" className="fgs-ali">
             Services
           </Link>
-          <Link path="/about" className="fgs-ali">
+          <Link path="/about" target="_blank" className="fgs-ali">
             About
           </Link>
-          <Link path="/careers" className="fgs-ali">
+          <Link path="/careers" target="_blank" className="fgs-ali">
             Careers
           </Link>
-          <Link path="/contact" className="fgs-ali">
+          <Link path="/contact" target="_blank" className="fgs-ali">
             Contact
           </Link>
         </div>
 
         <div className="resources-links caption-medium fgs-ali fgs-al fgs-al-v fgs-al-g-6">
           <span className="links-title fgs-ali">RESOURCES</span>
-          <Link path="/blog" className="fgs-ali">
+          <Link path="/blog" target="_blank" className="fgs-ali">
             Blog
           </Link>
-          <Link path="/news" className="fgs-ali">
+          <Link path="/news" target="_blank" className="fgs-ali">
             News
           </Link>
-          <Link path="/playbook" className="fgs-ali">
+          <Link path="/playbook" target="_blank" className="fgs-ali">
             Playbook
           </Link>
         </div>
@@ -57,10 +57,10 @@ const Footer = () => (
           <span className="links-title fgs-ali">FOLLOW US</span>
 
           <div className="fgs-ali fgs-al fgs-al-h fgs-al-g-20 fgs-al-align-items-flex-center">
-            <Link path="https://www.youtube.com/user/bitesiteca">
+            <Link path="https://www.youtube.com/user/bitesiteca" target="_blank">
               <FontAwesomeIcon icon={faYoutube} className="fa-2x" />
             </Link>
-            <Link path="https://www.twitter.com/bitesite">
+            <Link path="https://www.twitter.com/bitesite" target="_blank">
               <FontAwesomeIcon icon={faTwitter} className="fa-2x" />
             </Link>
           </div>
