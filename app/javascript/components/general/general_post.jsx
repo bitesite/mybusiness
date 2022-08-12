da200350f13345a3c76133e4b0e8de5d2a0c3d18
Nar Raeewal <nraeewal@Nars-MacBook-Pro.local>
2022-08-12 10:01:59 -0600

    pr comments

--- app/javascript/components/general/general_post.jsx
+++ app/javascript/components/general/general_post.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GeneralPost = ({ image, header, text, buttonText, link, positionImageRight }) => (
-  <div className="general-component-card fgs-al fgs-al-v fgs-al-g-30">
-    <div className="content-block fgs-al fgs-ali fgs-al-h fgs-al-g-60 fgs-al-align-items-center fgs-al-justify-content-space-evenly">
-      <div className={`image-block fgs-ali ${positionImageRight ? 'right' : ''}`}>
-        <img className="card-image" src={image} alt="general component" />
-      </div>
+const GeneralPost = ({ image, header, text, buttonText, link, positionImageRight, buttonShow, buttonClass }) => {
+  const handleButtonClick = () => window.open(link);
 
-      <div className="content fgs-al fgs-ali fgs-al-v fgs-al-g-30 ">
-        <div className="title fgs-ali heading-regular fgs-al-g-16">{header}</div>
-        <p className="text fgs-ali">{text}</p>
-        <a href={link}>
-          <button className="button fgs-ali btn primary-default" type="button">
-            {buttonText}
-          </button>
-        </a>
+  return (
+    <div className="general-component-card fgs-al fgs-al-v fgs-al-g-30">
+      <div className="content-block fgs-al fgs-ali fgs-al-h fgs-al-g-60 fgs-al-align-items-center fgs-al-justify-content-space-evenly">
+        <div className={`image-block fgs-ali ${positionImageRight ? 'right' : ''}`}>
+          <img className="card-image" src={image} alt="general component" />
+        </div>
+
+        <div className="content fgs-al fgs-ali fgs-al-v fgs-al-g-30 ">
+          <div className="title fgs-ali heading-regular fgs-al-g-16">{header}</div>
+          <p className="text fgs-ali">{text}</p>
+          {buttonShow && (
+            <button className={`button fgs-ali btn ${buttonClass}`} type="button" onClick={handleButtonClick}>
+              {buttonText}
+            </button>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
+GeneralPost.defaultProps = {
+  image: PropTypes.string,
+  header: PropTypes.string,
+  text: PropTypes.string,
+  buttonText: PropTypes.string,
+  link: PropTypes.string,
+  positionImageRight: true,
+  buttonShow: true,
+  buttonClass: 'primary-default',
+};
 
 GeneralPost.propTypes = {
   image: PropTypes.string,
@@ -28,6 +43,8 @@ GeneralPost.propTypes = {
   buttonText: PropTypes.string,
   link: PropTypes.string,
   positionImageRight: PropTypes.bool,
+  buttonShow: PropTypes.bool,
+  buttonClass: PropTypes.string,
 };
 
 export default GeneralPost;
