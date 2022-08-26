2f4196db6dd5269c106053bf7d6c216f0a33aec0
Nar Raeewal <nar.raeewal@bitesite.ca>
2022-08-26 08:09:44 -0600

    moved onclick to props

--- app/javascript/components/general/general_post.jsx
+++ app/javascript/components/general/general_post.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GeneralPost = ({ image, header, text, buttonText, link, positionImageRight, buttonShow, buttonClass }) => {
-  const handleButtonClick = () => window.open(link);
+const GeneralPost = ({ image, header, text, buttonText, link, positionImageRight, buttonHide, buttonClass, onClick }) => (
+  <div className="general-component-card fgs-al fgs-al-v fgs-al-g-30">
+    <div className="content-block fgs-al fgs-ali fgs-al-h fgs-al-g-60 fgs-al-align-items-center fgs-al-justify-content-space-evenly">
+      <div className={`image-block fgs-ali ${positionImageRight ? 'right' : ''}`}>
+        <img className="card-image" src={image} alt="general component" />
+      </div>
 
-  return (
-    <div className="general-component-card fgs-al fgs-al-v fgs-al-g-30">
-      <div className="content-block fgs-al fgs-ali fgs-al-h fgs-al-g-60 fgs-al-align-items-center fgs-al-justify-content-space-evenly">
-        <div className={`image-block fgs-ali ${positionImageRight ? 'right' : ''}`}>
-          <img className="card-image" src={image} alt="general component" />
-        </div>
-
-        <div className="content fgs-al fgs-ali fgs-al-v fgs-al-g-30 ">
-          <div className="title fgs-ali heading-regular fgs-al-g-16">{header}</div>
-          <p className="text fgs-ali">{text}</p>
-          {buttonShow && (
-            <button className={`button fgs-ali btn ${buttonClass}`} type="button" onClick={handleButtonClick}>
-              {buttonText}
-            </button>
-          )}
-        </div>
+      <div className="content fgs-al fgs-ali fgs-al-v fgs-al-g-30 ">
+        <div className="title fgs-ali heading-regular fgs-al-g-16">{header}</div>
+        <p className="text fgs-ali">{text}</p>
+        {!buttonHide && (
+          <button className={`button fgs-ali btn ${buttonClass}`} type="button" onClick={onClick}>
+            {buttonText}
+          </button>
+        )}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 GeneralPost.defaultProps = {
   image: PropTypes.string,
@@ -32,8 +28,8 @@ GeneralPost.defaultProps = {
   buttonText: PropTypes.string,
   link: PropTypes.string,
   positionImageRight: true,
-  buttonShow: true,
-  buttonClass: 'primary-default',
+  buttonHide: false,
+  onClick: () => {},
 };
 
 GeneralPost.propTypes = {
@@ -43,8 +39,9 @@ GeneralPost.propTypes = {
   buttonText: PropTypes.string,
   link: PropTypes.string,
   positionImageRight: PropTypes.bool,
-  buttonShow: PropTypes.bool,
+  buttonHide: PropTypes.bool,
   buttonClass: PropTypes.string,
+  onClick: () => {},
 };
 
 export default GeneralPost;
