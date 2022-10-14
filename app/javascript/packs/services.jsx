fc5f9a3cc0908d26f0de510f458737a4a9d013e0
Angela Choi <angelamchoi@gmail.com>
2022-10-14 10:46:56 -0400

    worked on services page

--- app/javascript/packs/services.jsx
+++ app/javascript/packs/services.jsx
@@ -10,12 +10,12 @@ const ServicesPage = () => (
       <h1>We work with you to build custom software and web applications.</h1>
       <div className="product-management">
         <h2>Product Management</h2>
-        <p className="text">
+        <p className="text-left-align">
           Product management is all about deciding what to build, and when to build it. We'll help you decide what's important and
           what's not and priortize the features that will make the most impact in your product.
         </p>
-        <div className="services-image">
-          <img src={productManagementImg} alt="product-managment" />
+        <div className="product-management-right-align">
+          <img src={productManagementImg} alt="product-managment" className="img-right" />
         </div>
       </div>
       <div className="software-development">
@@ -25,19 +25,19 @@ const ServicesPage = () => (
           viable software. Everything from using modern languages and frameworks, to best practices around source control and
           deployment, we'll build a solution that suits you perfectly.
         </p>
-        <div className="services-image">
-          <img src={softwareDevImg} alt="software-dev" />
+        <div className="software-dev">
+          <img src={softwareDevImg} alt="software-dev" className="img-left" />
         </div>
       </div>
       <div className="ux/ui-design">
         <h2>UX/UI Design</h2>
-        <p className="text">
+        <p className="text-left-align">
           Whether it be a 5-minute conversation or full-blown prototypes, taking some time to think about how your users will
           interact with your app can make a big difference. We'll work with you to design the best user experience for your
           customers.
         </p>
-        <div className="services-image">
-          <img src={uxUiDesignImg} alt="uxui-design" />
+        <div className="design-right-align">
+          <img src={uxUiDesignImg} alt="uxui-design" className="img-right" />
         </div>
       </div>
     </div>
